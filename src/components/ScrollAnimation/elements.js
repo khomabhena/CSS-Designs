df59cd350c026e986e4a02353c2ff557114c13cb
Kholwani Mabhena <colwanymab@gmail.com>
2022-06-12 22:01:53 +0200

    Elements js changed

---
+++ src/components/ScrollAnimation/elements.js
@@ -0,0 +1,13 @@
+import styledComponents from "styled-components";
+
+export const ScrollContainer = styledComponents.div`
+    
+`
+
+export const Content = styledComponents.div`
+
+`
+
+export const H1 = styledComponents.h1``
+
+export const H2 = styledComponents.h1``
